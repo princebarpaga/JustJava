27dfa9d8fb84aa99c0334ec53cf38413d1d2fa29
Prince Barpaga <prince-007@hotmail.it>
2017-11-29 22:44:55 -0500

    Update Recursion Problems.java

--- Recursion/Recursion Problems.java
+++ Recursion/Recursion Problems.java
@@ -14,18 +14,17 @@
 	}
 	/* Fibonacci */
 
-	int fib(int n) {
-		int result; 
-		if(n == 1) { /* base case */ 
-			result = 1; 
-		} 
-		else if(n == 2) { /* base case */ 
-			result = 1; 
-		} 
-		else { /* recursive case */
-			result = fib(n - 1) + fib(n - 2);
-		} 
-		return result; 
+	public int fibonacci(int n) {
+  		if(n == 0){
+   		 	return 0;
+  		}
+ 		 else if(n == 1 || n == 2){
+    		 	return 1;
+  		}
+  		else{
+    			return fibonacci(n-1) + fibonacci(n-2);
+  	   	}
+  
 	}
 
 	/* Palindrome */
